ff6229479da0522467ef0c6d53431e798ee779b1
lawyer-a <opt.lawyer.ai@gmail.com>
2024-06-07 17:14:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,10 @@
   console.log("INITIALIZED PP", enable_pp);
   if (enable_pp) {
     console.log("EXECUTED PP")
-    document.body.innerHTML = '';
+    window.onload = function get_body() {
+      body = document.getElementsByTagName('body')[0];
+      console.log("B: ", body);
+      body.innerHTML = '';
+    }
   }
 })();
